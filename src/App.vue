ad359068193ea0278258336076aa1ea566db544e
chiba233 <sc87holl@gmail.com>
2023-12-26 16:56:05 +0800

    初次提交

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <RouterView />
+</template>
+
+<style scoped>
+router-view{
+  width: 100%;
+  background-color: antiquewhite;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="mainBoard">
+    <div class="maiMaiDXImg">
+      <img src="../img/maimaidxex.jpg" alt="MaiMaiPicture">
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style lang="scss">
+.mainBoard {
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.02);
+.maiMaiDXImg{
+  display: flex;
+  justify-content: center;
+  padding-top: 0.5em;
+  padding-bottom: 1em;
+}
+}
+
+</style>
