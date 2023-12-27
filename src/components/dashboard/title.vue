2f71a2d877005676311f979af60c7160c8e7e631
chiba233 <sc87holl@gmail.com>
2023-12-27 21:13:23 +0800

    更新

---
+++ src/components/dashboard/title.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="main">
+    <div class="title">
+      <img src="../../img/logo.png" alt="logo">
+    </div>
+    <div class="setButton" @click="openURL('https://baidu.com/')">
+      <img src="../../img/btn_setting.png" alt="setting">
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+function openURL(url: string) {
+  window.open(url)
+}
+
+</script>
+
+<style lang="scss">
+.main {
+  display: flex;
+  padding-top: 2em;
+  justify-content: center;
+
+  .title {
+    position: absolute;
+
+    img {
+      height: 5em;
+      width: 15em;
+    }
+  }
+
+  .setButton {
+    width: 25em;
+    height: 5em;
+    display: flex;
+    justify-content: flex-end;
+    align-items: flex-end;
+    position: absolute;
+
+    img {
+      height: 2em;
+      width: 2em;
+    }
+  }
+}
+
+
+</style>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,12 +1,23 @@
 <template>
-  <a>ttttt</a>
+  <div class="backBody">
+    <Title></Title>
+  </div>
 </template>
-
-<style>
-
-</style>
 
 
 <script setup lang="ts">
+
+import Title from "@/components/dashboard/title.vue";
 </script>
 ```
+<style>
+.backBody {
+  width: 100%;
+  height: 100%;
+  background-color: #51bcf3;
+  justify-content: center;
+  text-align: center;
+  display: flex;
+
+}
+</style>
