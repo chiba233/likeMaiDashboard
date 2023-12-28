4b965db0337d5eb3954afac7ab48202eff96e4ab
chiba <sc87holl@gmaiil.com>
2023-12-28 22:49:32 +0800

    fix svg bug

--- src/components/dashboard/menu.vue
+++ src/components/dashboard/menu.vue
@@ -8,40 +8,40 @@
     </svg>
     <div class="list1">
       <div class="homeSelect">
-        <img src="../../img/menu/menu_home_on.png" v-if="menuSelect==0" alt="home_on" @click="menuSelectFun(0)">
+        <img src="../../img/menu/menu_home_on.png" v-if="menuSelect===0" alt="home_on" @click="menuSelectFun(0)">
         <img src="../../img/menu/menu_home.png" v-if="menuSelect!==0" alt="home" @click="menuSelectFun(0)">
       </div>
       <div class="playData">
         <img src="../../img/menu/menu_playerdata.png" v-if="menuSelect!==1" alt="playData" @click="menuSelectFun(1)">
-        <img src="../../img/menu/menu_playerdata_on.png" v-if="menuSelect== 1" alt="playData_on"
+        <img src="../../img/menu/menu_playerdata_on.png" v-if="menuSelect=== 1" alt="playData_on"
              @click="menuSelectFun(1)">
       </div>
       <div class="friends">
         <img src="../../img/menu/menu_friend.png" v-if="menuSelect!==2" alt="friends" @click="menuSelectFun(2)">
-        <img src="../../img/menu/menu_friend_on.png" v-if="menuSelect==2" alt="friends_on" @click="menuSelectFun(2)">
+        <img src="../../img/menu/menu_friend_on.png" v-if="menuSelect===2" alt="friends_on" @click="menuSelectFun(2)">
       </div>
       <div class="photos">
         <img src="../../img/menu/menu_photo.png" v-if="menuSelect!==3" alt="photos" @click="menuSelectFun(3)">
-        <img src="../../img/menu/menu_photo_on.png" v-if="menuSelect==3" alt="photos_on" @click="menuSelectFun(3)">
+        <img src="../../img/menu/menu_photo_on.png" v-if="menuSelect===3" alt="photos_on" @click="menuSelectFun(3)">
       </div>
     </div>
     <div class="list2">
       <div class="recodes">
         <img src="../../img/menu/menu_record.png" v-if="menuSelect!==4" alt="record" @click="menuSelectFun(4)">
-        <img src="../../img/menu/menu_record_on.png" v-if="menuSelect==4" alt="record_on" @click="menuSelectFun(4)">
+        <img src="../../img/menu/menu_record_on.png" v-if="menuSelect===4" alt="record_on" @click="menuSelectFun(4)">
       </div>
       <div class="events">
         <img src="../../img/menu/menu_map.png" v-if="menuSelect!==5" alt="record" @click="menuSelectFun(5)">
-        <img src="../../img/menu/menu_map_on.png" v-if="menuSelect==5" alt="record_on" @click="menuSelectFun(5)">
+        <img src="../../img/menu/menu_map_on.png" v-if="menuSelect===5" alt="record_on" @click="menuSelectFun(5)">
       </div>
       <div class="collection">
         <img src="../../img/menu/menu_collection.png" v-if="menuSelect!==6" alt="collection" @click="menuSelectFun(6)">
-        <img src="../../img/menu/menu_collection_on.png" v-if="menuSelect==6" alt="collection_on"
+        <img src="../../img/menu/menu_collection_on.png" v-if="menuSelect===6" alt="collection_on"
              @click="menuSelectFun(6)">
       </div>
       <div class="rank">
         <img src="../../img/menu/menu_ranking.png" v-if="menuSelect!==7" alt="collection" @click="menuSelectFun(7)">
-        <img src="../../img/menu/menu_ranking_on.png" v-if="menuSelect==7" alt="collection_on"
+        <img src="../../img/menu/menu_ranking_on.png" v-if="menuSelect===7" alt="collection_on"
              @click="menuSelectFun(7)">
       </div>
     </div>
@@ -61,7 +61,9 @@ import {menuSelectFun} from "@/components/ts/normal";
 
 <style scoped lang="scss">
 .menuBox {
-  width: 30em;
+  display: flex;
+  flex-direction: column;
+  width: 28.5em;
   padding: 0.5em;
 
   img {
